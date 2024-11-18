26e4c9834c722944b0dab9759afe48b0fda73f59
Uwihanganye obed <uwihanganyeobed@gmail.com>
2024-11-18 10:28:18 +0200

    added a new third route

--- server.js
+++ server.js
@@ -8,6 +8,38 @@ app.get('/user', (req,res)=>{
   res.send("This is users route")
 });
 
+app.get('/products',(req,res)=>{
+  const products = [
+    {
+      id: 1,
+      name: 'carrots',
+      price: 20
+    },
+    {
+      id: 2,
+      name: 'pizza',
+      price: 24
+    },
+    {
+      id: 3,
+      name: 'yams',
+      price: 12
+    },
+    {
+      id: 4,
+      name: 'maize',
+      price: 31
+    },
+    {
+      id: 5,
+      name: 'beans',
+      price: 22
+    },
+
+  ]
+  // res.send("These are list of products")
+  res.json(products);
+})
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
